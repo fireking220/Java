2896b1756b6829494a195748b48a46065fb8e4f2
Scott Patterson <sip2@pdx.edu>
2018-07-25 16:36:24 -0700

    reated Maze project

---
+++ BFS Maze/src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int[][] myMaze = new int[Integer.parseInt(args[0])][Integer.parseInt(args[1])]; //create a 2d array with input from args
+    }
+}
